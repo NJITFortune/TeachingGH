240c8eeb3920a0388d65a76f541cc5d33a3fa22a
Eric Fortune <eric.fortune@njit.edu>
2019-05-21 12:34:00 -0400

    / ‘SignalProc/manual_spec_plot.R’

--- SignalProc/manual_spec_plot.R
+++ SignalProc/manual_spec_plot.R
@@ -33,6 +33,8 @@ min(z)
 
 
 drericfortunesperfectspectrogramplottingfunction = function(freq_data, Fs, nfft, wl, ovlp) {
+  #nfft, wl are input in points
+  #ovlp is input in percent 
   
   #check if sample rate is given, if not extract from .wav file
   if(missing(Fs)) {
@@ -41,6 +43,26 @@ drericfortunesperfectspectrogramplottingfunction = function(freq_data, Fs, nfft,
     Fs = Fs
   }
   
+  #checks if nfft is given, if not substitutes default value
+  if(missing(nfft)) {
+    nfft = 1024
+  } else {
+    nfft = nfft
+  }
+  
+  #checks if wl is given, if not substitutes default value
+  if(missing(wl)) {
+    wl = 256
+  } else {
+    wl = wl
+  }
+  
+  #checks if ovlp is given, if not substitutes default value
+  if(missing(ovlp)) {
+    ovlp = 128
+  } else {
+    ovlp = (ovlp/100) * wl
+  }
   #check to see if input file is .wav.
   #if .wav, extract freq data, else directly use the data provided
   if(isS4(freq_data) == TRUE) {
@@ -48,12 +70,28 @@ drericfortunesperfectspectrogramplottingfunction = function(freq_data, Fs, nfft,
   } else {
     snd = freq_data
   }
+
+  #remove offset
+  snd = snd - mean(snd)
+  
   #produce spectrogram of data using signal function  
   ##MUST HAVE SIGNAL INSTALLED 
   spec = specgram(snd, nfft, Fs, wl, ovlp)
   
+  #remove phase info
+  P = abs(spec$S)
+  #normalize
+  P = P/max(P)
+  #convert to dB
+  P = 10*log10(P)
+  #extract time
+  t = spec$t
+  
+  image(x = t, y = spec$f, z = t(P), 
+        zlim = c(-35, -5),
+        col = (100), 
+        ylab = "Frequency [Hz]", 
+        xlab = "Time [s]")
 }
 
-typeof(zfinch_data)
-isS4(zfinch_data)
-isS4(zfinch_data@left)
+
