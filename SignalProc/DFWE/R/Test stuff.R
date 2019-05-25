a0f9085d2948c071038bb7e1f413ee0417a6647c
Alfred Amendolara <aba39@njit.edu>
2019-05-25 13:02:07 -0400

    / ‘SignalProc/DFWE/R/Test stuff.R’

--- SignalProc/DFWE/R/Test stuff.R
+++ SignalProc/DFWE/R/Test stuff.R
@@ -1,16 +1,12 @@
-document()
-library(tuneR)
-library(ggplot2)
-library(oce)
-library(signal)
-install.packages("roxygen2")
-library(roxygen2)
-install.packages("devtools")
-library(devtools)
 bat_sounds = readWave("Myotis_nattereri_1_o.wav")
-sepsyll(bat_sounds)
 zfinch_data = readWave("zfinch.wav")
-zf_syllables = sepsyll(zfinch_data, thresh = 100, plot_thresh = FALSE, syllable_filter = TRUE)
+
+
+zf_syllables = sepsyll(zfinch_data,
+                       thresh = 100,
+                       plot_thresh = FALSE,
+                       syllable_filter = TRUE)
+
 bat_syllables = sepsyll(bat_sounds,
                         syllable_filter = TRUE,
                         sms = 0.0002,
@@ -18,20 +14,8 @@ bat_syllables = sepsyll(bat_sounds,
                         plot_syl = FALSE,
                         index_simp = TRUE)
 
-ggplot() +
-  geom_line(aes(y = bat_syllables$syllable4, x = bat_syllables$timmy4), colour = "Blue")
-warnings()
 
-getclicks(zfinch_data)
 
-zfinch_data@samp.rate
-specplot(zfinch_data)
-plot(bat_sounds@left, type = "l")
 
-specplot(bat_syllables$syllable1, Fs = bat_sounds@samp.rate, ovlp = 95, nfft = 512)
-specplot(bat_syllables$syllable2, Fs = bat_sounds@samp.rate, ovlp = 95, nfft = 512)
-specplot(bat_syllables$syllable3, Fs = bat_sounds@samp.rate, ovlp = 95, nfft = 512)
-specplot(bat_syllables$syllable19, Fs = bat_sounds@samp.rate, ovlp = 95, nfft = 512)
-specplot(bat_syllables$syllable20, Fs = bat_sounds@samp.rate, ovlp = 95, nfft = 512)
 
-traceback()
+
