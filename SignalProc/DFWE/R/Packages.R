1457abfc920e6a6f9f054f614177f76cfc1dfcc3
Alfred Amendolara <aba39@njit.edu>
2019-05-25 13:02:15 -0400

    + ‘SignalProc/DFWE/R/Packages.R’

---
+++ SignalProc/DFWE/R/Packages.R
@@ -0,0 +1,16 @@
+install.packages("tuneR")
+install.packages("ggplot2")
+install.packages("oce")
+install.packages("signal")
+install.packages("roxygen2")
+install.packages("devtools")
+install.packages("dplyr")
+
+
+library(tuneR)
+library(ggplot2)
+library(oce)
+library(signal)
+library(roxygen2)
+library(devtools)
+library(dplyr)
