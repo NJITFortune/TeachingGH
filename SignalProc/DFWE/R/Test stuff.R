c09518582a8945921b0b14a3ab0087063442dbe8
Alfred Amendolara <aba39@njit.edu>
2019-05-25 13:17:21 -0400

    / ‘SignalProc/DFWE/R/Test stuff.R’

--- SignalProc/DFWE/R/Test stuff.R
+++ SignalProc/DFWE/R/Test stuff.R
@@ -2,20 +2,5 @@ bat_sounds = readWave("Myotis_nattereri_1_o.wav")
 zfinch_data = readWave("zfinch.wav")
 
 
-zf_syllables = sepsyll(zfinch_data,
-                       thresh = 100,
-                       plot_thresh = FALSE,
-                       syllable_filter = TRUE)
-
-bat_syllables = sepsyll(bat_sounds,
-                        syllable_filter = TRUE,
-                        sms = 0.0002,
-                        syl_filt = .002,
-                        plot_syl = FALSE,
-                        index_simp = TRUE)
-
-document()
-roxygen2::roxygenize()
-
 
 
