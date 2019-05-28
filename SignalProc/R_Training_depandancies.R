05b9261bd24e529773e36ff1841bc339b8c5b67c
Eric Fortune <eric.fortune@njit.edu>
2019-05-28 16:31:13 -0400

    + ‘SignalProc/R_Training_depandancies.R’

---
+++ SignalProc/R_Training_depandancies.R
@@ -0,0 +1,23 @@
+install.packages("signal")
+install.packages("tuneR") #for reading .wav files
+install.packages("ggplot2") # for plotting 
+install.packages("oce") #plot images 
+install.packages("seewave") #handling audio data
+install.packages("gridExtra") #for formatting ggplot graphs
+devtools::install_github("NJITFortune/DFWE") #custom package for plotting spectrogram
+
+library(signal)
+library(tuneR)
+library(ggplot2)
+library(oce)
+library(seewave)
+library(gridExtra)
+library(DFWE)
+
+<pre class="r"><code>library(signal)</code>
+  <code>library(tuneR)</code>
+  <code>library(ggplot2)</code>
+  <code>library(oce)</code>
+  <code>library(seewave)</code>
+  <code>library(gridExtra)</code>
+  <code>library(DFWE)</code></pre>
