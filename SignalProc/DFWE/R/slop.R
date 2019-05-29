50ad793f2d04ae9b0b610a9728807a0cc8d0301e
Eric Fortune <eric.fortune@njit.edu>
2019-05-29 13:36:05 -0400

    / ‘SignalProc/DFWE/R/slop.R’

--- SignalProc/DFWE/R/slop.R
+++ SignalProc/DFWE/R/slop.R
@@ -15,14 +15,33 @@ slop = function(wav_file, syllable_data, Fs, ...) {
     wav_file = wave_file
   }
 
-  #check to see if syllable start and end data is provided
-  if missing(syllable_data){
-    specplot(wav_file, ...)
+  #generate dominant frequency, check to see if dom_freq is set to TRUE
+  if(dom_freq){
 
+    #check to see if syllable start and end data is provided
+    if(missing(syllable_data)) {
+      #plot just spectro and dominant frequency if no syllable start or end data
+      specplot(wav_file, Fs)
+      par(new = TRUE)
+      plot()
+      dfreq(wav_file, Fs, ovlp = 90, type = "l")
+
+    } else {
+      #create list of start and end data for syllables
+      starts = syllable_data@syllable_start/Fs
+      ends = syllable_data@syllable_end/Fs
+      #plot
+      specplot(wav_file, Fs)
+      abline(v = starts, col = "Green", lwd = 2)
+      abline(v = ends, col = "Red", lwd = 2)
+    }
   } else {
+    #if dominant frequency is not choosen, plot without
+    #create list of start and end data for syllables
     starts = syllable_data@syllable_start/Fs
     ends = syllable_data@syllable_end/Fs
-    specplot(wav_file, ...)
+    #plot using ablines and list of times
+    specplot(wav_file, Fs)
     abline(v = starts, col = "Green", lwd = 2)
     abline(v = ends, col = "Red", lwd = 2)
   }
