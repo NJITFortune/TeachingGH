d3f8677a0aca0716ce6b6f8195899e8288b85da2
Eric Fortune <eric.fortune@njit.edu>
2019-05-30 13:30:27 -0400

    / ‘SignalProc/DFWE/R/slop.R’

--- SignalProc/DFWE/R/slop.R
+++ SignalProc/DFWE/R/slop.R
@@ -7,17 +7,34 @@
 #'
 #' @usage slop(zfinch_data, zf_syllables)
 #'
-#' @param wav_file
-#' @param Fs
-#' @param syllable_data
-#' @param dom_freq
-#' @param ...
+#' @param wav_file A .wav file or list of frequency data
+#' @param syllable_data A data frame produced by the sepsyll function
+#' @param Fs Sample rate, optional if included in .wav file
+#' @param nfft Default value 128 when bat = TRUE, when bat = FALSE the default is 512
+#' @param dom_freq defaults to TRUE, includes dominate frequency plot
+#' @param bat Defaults to TRUE, a series of default values tuned to produce nice plots from bat data
 #'
 #' @examples
 #'
 #' @export
 
-slop = function(wav_file, Fs, syllable_data, dom_freq = FALSE, ...) {
+slop = function(wav_file, syllable_data, Fs, nfft, dom_freq = TRUE, bat = TRUE) {
+
+  #check for and set nfft
+  if(missing(nfft) & bat == FALSE ) {
+    nfft = 512
+  }
+  if(missing(nfft) & bat == TRUE) {
+    nfft = 128
+  } else {
+    nfft = nfft
+  }
+
+  if(bat) {
+    ar = c(-10,0)
+  } else {
+    ar =
+  }
 
   #check if sample rate is given, if not extract from .wav file
   if(missing(Fs)) {
@@ -31,7 +48,7 @@ slop = function(wav_file, Fs, syllable_data, dom_freq = FALSE, ...) {
   if(isS4(wav_file) == TRUE) {
     wav_file = wav_file@left
   } else {
-    wav_file = wave_file
+    wav_file = wav_file
   }
 
   #generate dominant frequency, check to see if dom_freq is set to TRUE
@@ -40,28 +57,48 @@ slop = function(wav_file, Fs, syllable_data, dom_freq = FALSE, ...) {
     #check to see if syllable start and end data is provided
     if(missing(syllable_data)) {
       #plot just spectro and dominant frequency if no syllable start or end data
-      specplot(wav_file, Fs)
+      specplot(wav_file, Fs, nfft = nfft, amp_range = ar)
       par(new = TRUE)
-      plot()
-      dfreq(wav_file, Fs, ovlp = 90, type = "l")
+      #dfreq function from seewave
+      dfreq(wav_file, f = Fs,
+            type = "l",
+            wl = nfft,
+            ovlp = 95,
+            xaxt = "none",
+            yaxt = "none",
+            xlab = "",
+            ylab = "",
+            col = "Blue",
+            lwd = 2)
 
     } else {
       #create list of start and end data for syllables
-      starts = syllable_data@syllable_start/Fs
-      ends = syllable_data@syllable_end/Fs
+      starts = syllable_data$syllable_start/Fs
+      ends = syllable_data$syllable_ends/Fs
       #plot
-      specplot(wav_file, Fs)
-      abline(v = starts, col = "Green", lwd = 2)
-      abline(v = ends, col = "Red", lwd = 2)
+      specplot(wav_file, Fs, nfft = nfft, amp_range = ar)
+      abline(v = starts, col = "Green", lwd = .5)
+      abline(v = ends, col = "Red", lwd = .5)
+      par(new = TRUE)
+      dfreq(wav_file, f = Fs,
+            type = "l",
+            wl = nfft,
+            ovlp = 95,
+            xaxt = "none",
+            yaxt = "none",
+            xlab = "",
+            ylab = "",
+            col = "Blue",
+            lwd = 2)
     }
   } else {
     #if dominant frequency is not choosen, plot without
     #create list of start and end data for syllables
-    starts = syllable_data@syllable_start/Fs
-    ends = syllable_data@syllable_end/Fs
+    starts = syllable_data$syllable_start/Fs
+    ends = syllable_data$syllable_ends/Fs
     #plot using ablines and list of times
-    specplot(wav_file, Fs)
-    abline(v = starts, col = "Green", lwd = 2)
-    abline(v = ends, col = "Red", lwd = 2)
+    specplot(wav_file, Fs, nfft = nfft, amp_range = ar)
+    abline(v = starts, col = "Green", lwd = .5)
+    abline(v = ends, col = "Red", lwd = .5)
   }
 }
