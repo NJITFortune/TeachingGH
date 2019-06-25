e35dcc04930d29ec29032c1007d1755f302d4353
Eric Fortune <eric.fortune@njit.edu>
2019-06-25 15:46:33 -0400

    / ‘SignalProc/DFWE/R/domplot.R’

--- SignalProc/DFWE/R/domplot.R
+++ SignalProc/DFWE/R/domplot.R
@@ -1,13 +1,23 @@
 #' domplot
 #'
-#' Plots the dominate frequency of a specrogram. Two options for dominate frequency functions are provided. One from seewave, which works
-#' nicely and one from   which attempts to mitagate jumps in the dominate frequency. This is useful when there are breaks in the
-#' syllable.
+#' Plots the dominate frequency of a specrogram. Uses seeWave dfreq function. Will plot directly on top of spectrogram. This function
+#' will plot and automatically output the matrix result of dfreq. This allows some other analysis to be done, for instance taking
+#' minimum/maximum or generating a linear regression.
 #'
+#' @param wav_file A .wav file or list of frequency data
+#' @param Fs Sample rate, optional if included in .wav file
+#' @param nfft Default value 128 when bat = TRUE, when bat = FALSE the default is 512
+#' @param amp_range Changes default mapping of colors to decible range
+#' @param bat Defaults to TRUE, a series of default values tuned to produce nice plots from bat data
+#' @param dfcol Sets color for dominant frequency points
+#' @param ... Pass parameters on to specplot function !!EXCEPT amp_range!!
 #'
+#' @examples domplot(bat_syllables$syl1, f = 250000, dfcol = "White")
+#'
+#' @export
 
 
-domplot = function(wav_file, Fs, nfft, amp_range, df_fix = TRUE, dfcol, bat = TRUE, ...) {
+domplot = function(wav_file, Fs, nfft, amp_range, dfcol, bat = TRUE, ...) {
 
   #check for and set nfft
   if(missing(nfft) && bat == FALSE ) {
@@ -52,19 +62,30 @@ domplot = function(wav_file, Fs, nfft, amp_range, df_fix = TRUE, dfcol, bat = TR
     dfcol = dfcol
   }
 
+  domf = dfreq(wave = wav_file,
+               f = Fs,
+               wl = nfft,
+               ovlp = 75,
+               threshold = 15,
+               plot = FALSE)
+
+  #linear regression model
+  reg1 = lm(domf[,2]~domf[,1])
+
+
   #plot spectro and dominant frequency
   specplot(wav_file, Fs, nfft = nfft, amp_range = amp_range, ...)
   par(new = TRUE)
   #dfreq function from seewave
-  dfreq(wav_file, f = Fs,
-        type = "l",
-        wl = nfft,
-        ovlp = 70,
-        xaxt = "none",
-        yaxt = "none",
-        xlab = "",
-        ylab = "",
-        col = dfcol,
-        lwd = 2,
-        threshold = 10)
+  plot.default(domf,
+               type = "p",
+               pch = 20,
+               xaxt = "none",
+               yaxt = "none",
+               xlab = "",
+               ylab = "",
+               col = dfcol)
+  abline(reg1, col = dfcol)
+
+  return(domf)
 }
