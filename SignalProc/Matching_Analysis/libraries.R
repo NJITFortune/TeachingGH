c53c907b7a9f117acaab35d7df60664e87d06e36
Eric Fortune <eric.fortune@njit.edu>
2019-08-07 14:39:43 -0400

    + ‘SignalProc/Matching_Analysis/libraries.R’

---
+++ SignalProc/Matching_Analysis/libraries.R
@@ -0,0 +1,9 @@
+install.packages("~/SparkleShare/github.com/TeachingGH/SignalProc/DFWE_0.1.2.tar.gz", repos = NULL)
+library(DFWE)
+library(oce)
+library(signal)
+library(seewave)
+library(tuneR)
+library(ggplot2)
+
+
