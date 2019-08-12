82d8f78d89031be84bde3d5d22590709c11cb688
Eric Fortune <eric.fortune@njit.edu>
2019-08-12 15:52:08 -0400

    / ‘SignalProc/Matching_Analysis/bird_analysis.R’

--- SignalProc/Matching_Analysis/bird_analysis.R
+++ SignalProc/Matching_Analysis/bird_analysis.R
@@ -2,5 +2,5 @@ clipping1 = readWave("~/Desktop/Matching2018/clippings_for_analysis/ZOOM0007_201
 
 specplot(clipping1, amp_range = c(-20,0), y_limit=c(0,5000))
 
-sepsyll()
+sepsyll(clipping1)
          
