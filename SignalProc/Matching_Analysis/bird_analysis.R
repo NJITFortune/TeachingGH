b94f83931fa3d7566f3bd108beb550b1ac88674d
Eric Fortune <eric.fortune@njit.edu>
2019-12-18 15:16:16 -0500

    / ‘SignalProc/Matching_Analysis/bird_analysis.R’

--- SignalProc/Matching_Analysis/bird_analysis.R
+++ SignalProc/Matching_Analysis/bird_analysis.R
@@ -4,3 +4,4 @@ specplot(clipping1, amp_range = c(-20,0), y_limit=c(0,5000))
 
 test_clippings = sepsyll(clipping1)
          
+specplot(test_clippings$syl23, amp_range = c(-20,0), Fs = 44100, y_limit =c(0,5000) )
