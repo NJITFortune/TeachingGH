7fbd247d74def8b45e4801e8ad81d6df80d8f721
Eric Fortune <eric.fortune@gmail.com>
2021-02-16 19:54:04 -0500

    / ‘puppies.m’

--- puppies.m
+++ puppies.m
@@ -188,6 +188,8 @@ function puppies(num, simulationlength, jigglestrength, biastrength)
 
     function newloc = puppycheck(struct, idx, kk)
 
+        jumpfactor = 3;
+        
         for pp = length(struct):-1:1
             shp(pp) = polyshape(struct(pp).coord(:,1), struct(pp).coord(:,2));
         end
@@ -197,9 +199,6 @@ function puppies(num, simulationlength, jigglestrength, biastrength)
         newloc = struct(idx).ctr(end,:); 
 
         if sum(TF) ~= 1  % There was an overlap! Do something!!
-        
-            whichidx = find(TF) 
-            whichidx = whichidx(whichidx~=idx)
             
             for qq = 1:length(whichidx)
                 
