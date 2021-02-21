5f630d522a4f918acc3fe8c2b60e8a6d99299f23
Eric Fortune <eric.fortune@njit.edu>
2021-02-21 09:14:45 -0500

    / ‘puppies.m’

--- puppies.m
+++ puppies.m
@@ -35,7 +35,7 @@ function puppies(num, simulationlength, jigglestrength, biastrength)
         for z = 1:num  % For each puppy
             
         % RANDOM PUPPY ANGLE for each step in the simulation
-        z
+        
         scottie(z).puppyang(k) = puppyturn(scottie(z).puppyang(k-1), maxturnangle, jigglestrength);
 
         % RANDOM PUPPY MOVEMENT for each step in the simulation
