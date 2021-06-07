e19f74027595ebb44fef033f54180b48bc161d99
Eric Fortune <eric.fortune@njit.edu>
2021-06-07 00:56:10 -0400

    / ‘NSB/makeStochResMovie.m’

--- NSB/makeStochResMovie.m
+++ NSB/makeStochResMovie.m
@@ -1,7 +1,35 @@
 a = imread('~/Downloads/Wrens2011-CarlosPhoto.jpeg');
 a = a(:,:,2);
 
+[im1, sr1] = StochRes2021(a, 
+
 vidfile = VideoWriter('StochRes2021.mp4','MPEG-4');
 
 open(vidfile);
 
+
+
+
+function [im, sr] = StochRes2021(in, thresh)
+
+im = in;
+
+x = length(im(:,1));
+y = length(im(1,:));
+
+im(in > thresh) = 255;
+im(in < thresh) = 0;
+
+sr = zeros(x, y, 'uint8');
+%threshbox = thresh * ones(x, y);
+rnd = randi([0 80], x, y, 'uint8');
+
+for j = 1:length(rnd(:,1))
+    for k = 1:length(rnd(1,:))
+        if in(j,k) > rnd(j,k)+thresh  
+            sr(j,k) = 255;
+        end
+    end
+end
+
+end
