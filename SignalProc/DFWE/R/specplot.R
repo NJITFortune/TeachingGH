e3a001f590696f4504ed63120abfe917bc3813a5
Eric Fortune <eric.fortune@njit.edu>
2022-05-14 21:24:12 -0400

    / ‘SignalProc/DFWE/R/specplot.R’

--- SignalProc/DFWE/R/specplot.R
+++ SignalProc/DFWE/R/specplot.R
@@ -1,15 +1,15 @@
 #' specplot
 #'
-#' This function will plot spectrograms. It works with either a wav file read by tuneR or frequency data.
-#' When using frequency data alone, you need to specify a sample frequency. This function relies on the
-#' imagep function from oce. specplot uses custom color palettes with 30 colors. Most parameters have defaults
-#' and do not need to be specified by the user. Can pass graphical arguements onto imagep.
+#' This function plots spectrograms. It works with a single channel, and requires data and samplerate.
+#' Objects from wav files have both data and samplerate.
+#' specplot exists to simplify making spectrograms that are pleasing to Dr. Fortune's eye.
+#' Most parameters have defaults that do not need to be specified by the user. Can pass graphical arguments onto imagep.
 #'
 #' @return A plot of a spectrgram using the default R graphics
 #'
-#' @usage specplot = function(freq_data, Fs, nfft, wl, ovlp, normal = TRUE, amp_range, color, amp_value = FALSE)
+#' @usage specplot = function(sdata, Fs, nfft, wl, ovlp, normal = TRUE, amp_range, color, amp_value = FALSE)
 #'
-#' @param freq_data Wav file or frequency data
+#' @param sdata Wav file or frequency data
 #' @param Fs Sample rate, not necessary if provided by .wav file
 #' @param nfft Defaults to 512
 #' @param wl Defaults to 1/2 nfft
@@ -29,7 +29,7 @@
 #'
 #' @export
 
-specplot = function(freq_data, Fs, nfft, wl, ovlp, normal = TRUE, amp_range, x_limit, y_limit, color, amp_value = FALSE, no_label = FALSE, ...) {
+specplot = function(sdata, Fs, nfft, wl, ovlp, normal = TRUE, amp_range, x_limit, y_limit, color, amp_value = FALSE, no_label = FALSE, ...) {
   #requires tuneR (if using wave file for input), signal (to produce spectro data), and oce (for plotting)
   #freq _data may be list of frequencies or wav file
   #if sample rate is provided in wav, it does not need to be specified, otherwise it MUST be given
@@ -44,82 +44,83 @@ specplot = function(freq_data, Fs, nfft, wl, ovlp, normal = TRUE, amp_range, x_l
   greyscale_custom = c("#FFFFFF","#F9F9F9","#F4F4F4","#F0F0F0","#EAEAEA","#E5E5E5","#E1E1E1","#DDDDDD","#D9D9D9","#D2D2D2","#CCCCCC","#C7C7C7","#C1C1C1","#BFBFBF","#BABABA","#B2B2B2","#A9A9A9","#9F9F9F","#949494","#8E8E8E","#818181","#747474","#696969","#636363","#585858","#4B4B4B","#3E3E3E","#333333","#202020","#000000")
   #check if sample rate is given, if not extract from .wav file
   if(missing(Fs)) {
-    Fs = freq_data@samp.rate
+    Fs = sdata@samp.rate
   } else {
     Fs = Fs
   }
 
 
-  #checks if nfft is given, if not substitutes default value
+  # Check for nfft. If not, use default (512)
   if(missing(nfft)) {
     nfft = 512
   } else {
     nfft = nfft
   }
 
-  #checks if wl is given, if not substitutes default value
+  # Check for wl (window length). If not, use default (nfft/2)
   if(missing(wl)) {
     wl = nfft/2
   } else {
     wl = wl
   }
 
-  #checks if ovlp is given, if not substitutes default value
+  # Checks for ovlp (overlap). If not, use default (50%)
   if(missing(ovlp)) {
     ovlp = wl/2
   } else {
     ovlp = (ovlp/100) * wl
   }
 
-  #check to see if input file is .wav.
-  #if .wav, extract freq data, else directly use the data provided
-  if(isS4(freq_data) == TRUE) {
-    snd = freq_data@left
+  # Check to see if input file is .wav.
+  # If .wav data, extract left channel sdata, else directly use the sdata provided
+  # This needs to be fixed.
+  if(isS4(sdata) == TRUE) {
+    snd = sdata@left
   } else {
-    snd = freq_data
+    snd = sdata
   }
 
-  #remove offset
+  # Remove DC offset
   snd = snd - mean(snd)
 
-  #produce spectrogram of data using signal function
-  ##MUST HAVE SIGNAL INSTALLED
+  # Calculate spectrogram
   spec = specgram(snd, nfft, Fs, wl, ovlp)
 
-  #remove phase info
+  # Remove phase info
   P = abs(spec$S)
 
-  #normalize
+  # Normalize if requested
   if(normal) {
     P = P/max(P)
   }
 
-  #convert to dB
-  P = 10*log10(P)
+  # Convert to dB
+    P = 10*log10(P)
 
-  #extract time
-  t = spec$t
+  # Extract time series
+    t = spec$t
 
-  #extract frequency for later
-  f = spec$f
+  # Extract frequency data
+    f = spec$f
 
-  #output amp values
+  # If requested, provide max and min amplitudes
+  # THIS DOESN"T SEEM CORRECT
   if(amp_value) {
     print(max(t(P)))
     print(min(t(P)))
   }
 
-
-  #check if user entered zlim parameter or set default
-  bottom_amp = 0.6
-  top_amp = 1
+  # Set amplitude range of spectrogram
   if(missing(amp_range)) {
+    bottom_amp = 0.6
+    top_amp = 1
     amp_range = c(min(t(P))*bottom_amp, max(t(P))*top_amp)
   } else {
     amp_range = amp_range
   }
 
-  #set color pallete, default is heat, 1 = greyscale, 2 = heat, 3 = rev heat, 4 = rev greyscale
+  # Set color palette (default is heat)
+  #  1 = greyscale, 2 = heat, 3 = reverse heat, 4 = revserse greyscale
   if(missing(color)) {
     col_select = rev(heat_col_custom)
     box_col = "Black"
@@ -128,6 +129,7 @@ specplot = function(freq_data, Fs, nfft, wl, ovlp, normal = TRUE, amp_range, x_l
     par(col.axis="black")
     par(col.main="black")
   } else {
+
     if(color == 3){
       col_select = greyscale_custom
       box_col = "Black"
@@ -165,24 +167,24 @@ specplot = function(freq_data, Fs, nfft, wl, ovlp, normal = TRUE, amp_range, x_l
     }
   }
 
-  #check to see if user input x limits
+  # Set time (x) limits
   if(missing(x_limit)) {
     x_limit = c(t[1], t[length(t)])
   } else {
     x_limit = x_limit
   }
 
-  #check to see if user input y limits
+  # Set frequency (y) limits
   if(missing(y_limit)) {
     y_limit = c(f[1], f[length(f)])
   } else {
     y_limit = y_limit
   }
-  
+
   if(no_label == FALSE)
   {
     #check to see frequency max
-    if(max(f) > 100000) {
+    if(max(f) > 2000) {
       #plot in kHz
       imagep(x = t, y = f, z = t(P),
              zlim = amp_range,
@@ -214,7 +216,7 @@ specplot = function(freq_data, Fs, nfft, wl, ovlp, normal = TRUE, amp_range, x_l
       box(col = box_col)
     }
   } else {
-    
+
     #plot without axis of frame
     imagep(x = t, y = f, z = t(P),
            zlim = amp_range,
@@ -230,7 +232,7 @@ specplot = function(freq_data, Fs, nfft, wl, ovlp, normal = TRUE, amp_range, x_l
            mar = rep(0, 4),
            ...)
     box(col = box_col)
-    
+
   }
-  
+
 }
